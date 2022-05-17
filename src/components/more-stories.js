6fe74b6612cd6e39ebc99da2f965674109c7f85f
Escaity <sfz2306@gmail.com>
2022-05-17 23:39:10 +0900

    タグ表示用に修正

--- src/components/more-stories.js
+++ src/components/more-stories.js
@@ -1,16 +1,16 @@
 import PostPreview from "./post-preview";
-import styles from "../styles/Home.module.scss"
+import styles from "../styles/Home.module.scss";
 // 記事一覧表示用
 export default function MoreStroies({ posts }) {
   return (
     <section className={styles.postList}>
-      <h2>記事一覧</h2>
       <div>
         {posts.map((blog) => (
           <PostPreview
             key={blog.body} /* keyがないとエラーになる */
             id={blog.id}
             title={blog.title}
+            tags={blog.tag}
             createdAt={blog.createdAt}
           />
         ))}
